71ec92197fb6f015dcf715009e31f41a9332d998
SwarupD21 <swarupsdash21@gmail.com>
2026-01-31 15:13:24 +0530

    Fix database config for Render PostgreSQL

--- password_vault_backend/settings.py
+++ password_vault_backend/settings.py
@@ -126,7 +126,9 @@ WSGI_APPLICATION = 'password_vault_backend.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL")
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=True,
     )
 }
 
